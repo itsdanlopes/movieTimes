c3bcdb8685ee20363b6bea93d9086552e341bc34
dloppes <d.loppes.soares@gmail.com>
2020-12-26 16:42:48 +0000

    apps & table changes

--- views/js/table.js
+++ views/js/table.js
@@ -25,8 +25,8 @@ function select_row()
 		$(".selected").removeClass("selected");
 		$(this).addClass("selected");
 		var section = $(this).prevAll("tr").children("td[colspan='3']").length - 1;
-		var entree = $(this).attr("id") - 1;
-		delete_row(section, entree);
+		var movie = $(this).attr("id") - 1;
+		delete_row(section, movie);
 	})
 };
 
