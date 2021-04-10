7e974cd949fd150589b0344f328826e095f239dd
dloppes <d.loppes.soares@gmail.com>
2021-04-10 21:27:12 +0100

    helmet & compression functions added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
 const routes = require('./routes.js');
 const app = express();
+
 
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(routes);
 app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(routes);
+
+
 
 app.get('/', (req, res) => res.json('Healthy check ok'));
 
