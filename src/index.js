eca09f9bf8cd6146771834deb0455dadc77c9b10
dloppes <d.loppes.soares@gmail.com>
2021-04-10 20:41:45 +0100

    correcting index.html table & adding cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes.js');
 const app = express();
 
@@ -6,6 +7,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(routes);
+app.use(cors);
 
 app.get('/', (req, res) => res.json('Healthy check ok'));
 
