7a860911f2753ee4525e7def5c086e2b981a3cc3
dloppes <d.loppes.soares@gmail.com>
2021-04-12 18:01:05 +0100

    constants compression, helmet and cors created

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
 const routes = require('./routes.js');
 const app = express();
 
@@ -6,6 +9,9 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(routes);
+app.use(cors());
+app.use(compression());
+app.use(helmet());
 
 app.get('/', (req, res) => res.json('Healthy check ok'));
 
