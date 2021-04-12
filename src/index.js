7f34546140d9d599d7c8993dc491b9ff5f61e41a
dloppes <d.loppes.soares@gmail.com>
2021-04-12 02:46:38 +0100

    index js

--- src/index.js
+++ src/index.js
@@ -1,45 +1,12 @@
-const listMovies = async() => {
+const express = require('express');
+const routes = require('./routes.js');
+const app = express();
 
-    const list = await listOfMovies();
-    const divList = document.getElementById("list-props");
+const port = process.env.PORT || 3000;
 
-    const listMovies = [];
-    list.forEach(element => { listMovies.push(element) });
+app.use(express.json());
+app.use(routes);
 
-    listMovies.forEach(item => {
-        const movie = createList(item);
-        divList.insertAdjacentHTML('afterbegin', movie);
-    });
-};
+app.get('/', (req, res) => res.json('Healthy check ok'));
 
-function calculateBill() {
-    let values = document.getElementById("item");
-
-    console.log(values.length)
-    for (let i = 0; i < 10; i++) {
-        console.log(values[0].value)
-    }
-}
-
-function insertMovie() {
-    const name = document.getElementById("name").value;
-    const genre = document.getElementById("genre").value;
-    const price = document.getElementById("price").value;
-    const duration = document.getElementById("duration").value;
-    const screen = document.getElementById("screen").value;
-    const showingTime = document.getElementById("showingTime").value;
-
-    const movie = {
-        name: name,
-        genre: genre,
-        price: price,
-        duration: duration,
-        screen: screen,
-        showingTime: showingTime
-    }
-
-    //createMovie(movie);
-    location.reload();
-}
-
-listMovies();
+app.listen(port, () => console.log('App running on port ', port));
---
+++ views/index.js
@@ -0,0 +1,45 @@
+const listMovies = async() => {
+
+    const list = await listOfMovies();
+    const divList = document.getElementById("list-props");
+
+    const listMovies = [];
+    list.forEach(element => { listMovies.push(element) });
+
+    listMovies.forEach(item => {
+        const movie = createList(item);
+        divList.insertAdjacentHTML('afterbegin', movie);
+    });
+};
+
+function calculateBill() {
+    let values = document.getElementById("item");
+
+    console.log(values.length)
+    for (let i = 0; i < 10; i++) {
+        console.log(values[0].value)
+    }
+}
+
+function insertMovie() {
+    const name = document.getElementById("name").value;
+    const genre = document.getElementById("genre").value;
+    const price = document.getElementById("price").value;
+    const duration = document.getElementById("duration").value;
+    const screen = document.getElementById("screen").value;
+    const showingTime = document.getElementById("showingTime").value;
+
+    const movie = {
+        name: name,
+        genre: genre,
+        price: price,
+        duration: duration,
+        screen: screen,
+        showingTime: showingTime
+    }
+
+    //createMovie(movie);
+    location.reload();
+}
+
+listMovies();
